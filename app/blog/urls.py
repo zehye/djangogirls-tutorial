7bf29a44ccbd7f8ecaf0d6c7888c0c342040a31e
parkjihye <zehye.01@gmail.com>
2018-06-01 16:08:04 +0900

    post-list 기본연결 구현
    config.urls에ㅓ r''(모든연결)을 blog.urls에서 처리하도록 include
    blog.urls 의 r'^$'(루트 URL, /)에 blog.view의 post-list view function을 연결

---
+++ app/blog/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from .views import post_list
+#from blog.views import post_list
+
+urlpatterns = [
+    # url의 첫번째 인자: 매치될 url 정규표현식
+    # 매칭에 성공하면 두번째 인자로 넘어간다: view function
+    # view function: request를 받아 response를 돌려주는 함수
+    # blog.views에 있는 post_list함수를 아래 url함수의 두번째 인자로 전달 (함수호출 아님)
+    url(r'^$', post_list),
+]
--- app/blog/views.py
+++ app/blog/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    return HttpResponse('')  # 특정 리퀘스트가 올떄 보통 http로 오고 여기로 응답을 보내는데 응답을 보내기 위한 무언가를 만들어줘
