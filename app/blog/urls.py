34ad80a241e4385e0620c98998afa1917201c5bc
parkjihye <zehye.01@gmail.com>
2018-06-05 12:01:42 +0900

    post_list에서 post_detail로 가는 링크생성

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     # 매칭에 성공하면 두번째 인자로 넘어간다: view function
     # view function: request를 받아 response를 돌려주는 함수
     # blog.views에 있는 post_list함수를 아래 url함수의 두번째 인자로 전달 (함수호출 아님)
-    url(r'^$', post_list),
+    url(r'^$', post_list, name='post-list'),
     # 정규표현식에 그룹을 지정해서 view function의 인수로 전달한다.
-    url(r'^(\d+)/', post_detail),
+    url(r'^/(\d+)/', post_detail, name='post-detail'),
+    # name은 url 자체의 이름
 ]
